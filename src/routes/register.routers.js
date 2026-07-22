78e4a6af883e8f81975747db1bd86cf89aec9c82
Mistu293-art <mistu14112006@gmail.com>
2026-07-22 10:17:21 +0530

    Replace project with edtech platform backend

--- src/routes/register.routers.js
+++
@@ -1,8 +0,0 @@
-
-import { Router } from "express";
-import {createUser} from "../controllers/register.controller.js"
-const router=Router();
-
-router.route("/register").post(createUser);
-
-export default router
--- src/routes/verify.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { verifyController } from "../controllers/verify.controller.js";
-
-const router = Router();
-
-router.get("/verify/:token", verifyController);
-
-export default router;
